2dd2fc3013e36f90d36677faa4d0e701b65a4528
Mohamad Reza Bozorgmehr <36222603+bozorgmehr1355@users.noreply.github.com>
2026-05-06 10:49:43 +0330

    Create profit.py

---
+++ services/profit.py
@@ -0,0 +1,16 @@
+from services.iron import get_fe65
+
+def calculate_profit(tonnage):
+    price = get_fe65()
+    freight = 25
+
+    revenue = price  tonnage
+    cost = freight  tonnage
+    profit = revenue - cost
+
+    return {
+        "price": price,
+        "revenue": revenue,
+        "cost": cost,
+        "profit": profit
+    }
