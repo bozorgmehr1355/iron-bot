660ca9150efeae403b4082e0e8d4871589341c18
Mohamad Reza Bozorgmehr <36222603+bozorgmehr1355@users.noreply.github.com>
2026-05-06 11:46:43 +0330

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 TOKEN = os.environ.get("BOT_TOKEN", "8742538592:AAHBoNMe33Wvsin73049sdi6htK-f3hEfUU")
 CHAT_ID = 715854466
-FRED_API_KEY = os.environ.get("FRED_API_KEY", "")
+FRED_API_KEY = os.environ.get("FRED_API_KEY", "7db3d61b038d240467ccdf404ce29cd0")
 
