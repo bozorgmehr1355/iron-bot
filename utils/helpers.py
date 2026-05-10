a4c6abaf9ed1cb8d82cc6e5516f389e0cf0cf997
Mohamad Reza Bozorgmehr <36222603+bozorgmehr1355@users.noreply.github.com>
2026-05-10 12:44:40 +0330

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,5 @@
-
+def to_persian_digits(text):
+    """تبدیل اعداد انگلیسی به فارسی"""
+    persian = {'0': '۰', '1': '۱', '2': '۲', '3': '۳', '4': '۴',
+               '5': '۵', '6': '۶', '7': '۷', '8': '۸', '9': '۹'}
+    return ''.join(persian.get(ch, ch) for ch in str(text))
